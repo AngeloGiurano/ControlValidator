3c6317417543bd26a003c0c57df7e1e8b2441b7c
Angelo Giurano <giuranoangelo@gmail.com>
2016-11-07 14:10:47 +0100

    Initial Commit

---
+++ ControlValidator/ControlValidator.h
@@ -0,0 +1,19 @@
+//
+//  ControlValidator.h
+//  ControlValidator
+//
+//  Created by Angelo Giurano on 11/7/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ControlValidator.
+FOUNDATION_EXPORT double ControlValidatorVersionNumber;
+
+//! Project version string for ControlValidator.
+FOUNDATION_EXPORT const unsigned char ControlValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ControlValidator/PublicHeader.h>
+
+
